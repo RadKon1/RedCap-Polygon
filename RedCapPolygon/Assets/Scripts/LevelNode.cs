0b7959c12e9c450d5cc816a3c4fae1ae25793ebe
colestan8 <s.hincz@outlook.com>
2026-04-01 23:23:54 +0200

    Created basic room generation, only with right oriented exits

---
+++ RedCapPolygon/Assets/Scripts/LevelNode.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoomType
+{
+    Start,
+    Combat,
+    UpgradeRoom,
+    Transition,
+    Boss
+}
+
+public class LevelNode
+{
+    public RoomType NodeType { get; private set; }
+    public List<LevelNode> NextNodes { get; private set; } = new List<LevelNode>();
+    public LevelNode(RoomType assignedType)
+    {
+        NodeType = assignedType;
+    }
+
+    public void AddNextNode(LevelNode node)
+    {
+        NextNodes.Add(node);
+    }
+}
---
+++ RedCapPolygon/Assets/Scripts/RoomController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    [SerializeField] private Transform entryPoint;
+    [SerializeField] private Transform[] exitPoints;
+    public Transform EntryPoint { get { return entryPoint; } }
+    public Transform[] ExitPoints { get { return exitPoints; } }
+}
