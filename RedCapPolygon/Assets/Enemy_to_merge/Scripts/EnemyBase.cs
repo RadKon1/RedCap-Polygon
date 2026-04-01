9e196023830771ed8f1a85030e7e2bf979a27184
Kra1ker <kirilplaysun@gmail.com>
2026-04-01 17:01:03 +0200

    Enemy files

---
+++ RedCapPolygon/Assets/Enemy_to_merge/Scripts/EnemyBase.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] private int health;
+    [SerializeField] private int damage;
+    [SerializeField] private float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
