79e5fd388b12f401ca65be928ffe781c31246028
Kra1ker <kirilplaysun@gmail.com>
2026-04-07 15:36:36 +0200

    Enemy (Linear)

--- RedCapPolygon/Assets/Enemy_to_merge/Scripts/EnemyBase.cs
+++ RedCapPolygon/Assets/Enemy_to_merge/Scripts/EnemyBase.cs
@@ -3,18 +3,128 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     [Header("Parameters")]
+    [SerializeField] private int maxHealth = 100;
     [SerializeField] private int health;
-    [SerializeField] private int damage;
-    [SerializeField] private float speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int damage = 3;
+    [SerializeField] private float speed = 2.4f;
+    [Header("References")]
+    private Rigidbody2D _rb;
+    private BoxCollider2D _col;
+    [SerializeField] private LayerMask groundMask;
 
-    // Update is called once per frame
+    private Vector2 _boxWallScale = new Vector2(2f, 1f);
+    private Vector3 _frontWallOffset = new Vector3(1.5f, 0f, 0f);
+    private Vector3 _frontWallCheckPos;
+    private Vector2 _boxEdgeScale = new Vector2(0.9f, 0.5f);
+    private Vector3 _edgeOffset = new Vector3(1.5f, -1f, 0f);
+    private Vector3 _edgeCheckPos;
+    private float _time = 0f;
+    private float _direction = 1f;
+    private float _rayLenght = 0.05f;
+    private bool isDead;
+    private bool isAttacking;
+    private bool onCooldown;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        health = maxHealth;
+        _rb = GetComponent<Rigidbody2D>();
+        _col = GetComponent<BoxCollider2D>();
+        groundMask = LayerMask.GetMask("Ground");
+    }
+    
     void Update()
     {
-        
+        _time += Time.deltaTime;
+        GroundCheck();
+        if (!isGrounded)
+        {
+            return;
+        }
+        WallCheck();
+        EdgeCheck();
+        Debug.Log("Moving");
+        Move();
     }
+
+    void Move()
+    {
+        _rb.linearVelocity = new Vector2(_direction * speed, _rb.linearVelocity.y);
+    }
+    
+    void Rotate()
+    {
+        if (_direction > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(_direction), 2, 1);
+        }
+        else if (_direction < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(_direction), 2, 1);
+        }
+    }
+
+    void Attack()
+    {
+        // Placeholder
+        isAttacking = true;
+    }
+    void TakeDamage(int damage)
+    {
+        // Placeholder
+        health -= damage;
+    }
+
+    #region  Collision
+    void GroundCheck()
+    {
+        // cRc - center RayCast, lRc - left RayCast, rRc - right RayCast
+        Vector2 cOrigin = _col.bounds.center - new Vector3(0, _col.bounds.extents.y, 0);
+        Vector2 lOrigin = _col.bounds.center - new Vector3(_col.bounds.extents.x, _col.bounds.extents.y, 0);
+        Vector2 rOrigin = _col.bounds.center + new Vector3(_col.bounds.extents.x, -_col.bounds.extents.y, 0);
+
+        RaycastHit2D cRc = Physics2D.Raycast(cOrigin, Vector2.down, _rayLenght, groundMask);
+        RaycastHit2D lRc = Physics2D.Raycast(lOrigin, Vector2.down, _rayLenght, groundMask);
+        RaycastHit2D rRc = Physics2D.Raycast(rOrigin, Vector2.down, _rayLenght, groundMask);
+        
+        Debug.DrawRay(cOrigin, Vector2.down * _rayLenght, Color.darkRed);
+        Debug.DrawRay(lOrigin, Vector2.down * _rayLenght, Color.darkRed);
+        Debug.DrawRay(rOrigin, Vector2.down * _rayLenght, Color.darkRed);
+        isGrounded = cRc.collider != null || lRc.collider != null || rRc.collider != null;
+        Debug.Log(isGrounded);
+    }
+
+    void WallCheck()
+    {
+        _frontWallCheckPos = transform.position + _frontWallOffset * _direction;
+        bool isFrontWallHit = Physics2D.OverlapBox(_frontWallCheckPos, _boxWallScale, 0, groundMask);
+        if (isFrontWallHit)
+        {
+            _direction *= -1;
+            Rotate();
+        }
+    }
+
+    void EdgeCheck()
+    {
+        _edgeCheckPos = transform.position + new Vector3 (_edgeOffset.x * _direction,_edgeOffset.y, _edgeOffset.z);
+        bool isEdgeHit = !Physics2D.OverlapBox(_edgeCheckPos, _boxEdgeScale, 0, groundMask);
+        if (isEdgeHit)
+        {
+            _direction *= -1;
+            Rotate();
+        }
+    }
+    void OnDrawGizmos()
+    {
+        if (_frontWallCheckPos == null || _edgeCheckPos == null)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(_frontWallCheckPos, _boxWallScale);
+        Gizmos.DrawWireCube(_edgeCheckPos, _boxEdgeScale);
+    }
+
+    #endregion
 }
