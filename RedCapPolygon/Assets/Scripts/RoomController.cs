938c601e7dbc29c46591bf2d53f874709468a601
colestan8 <s.hincz@outlook.com>
2026-04-18 22:34:38 +0200

    Początkowa wersja architektury generacji pokojów i wyliczania drzwi

--- RedCapPolygon/Assets/Scripts/RoomController.cs
+++ RedCapPolygon/Assets/Scripts/RoomController.cs
@@ -1,9 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
-    [SerializeField] private Transform entryPoint;
-    [SerializeField] private Transform[] exitPoints;
-    public Transform EntryPoint { get { return entryPoint; } }
-    public Transform[] ExitPoints { get { return exitPoints; } }
+    [System.Serializable]
+    public struct DoorMapping
+    {
+        [Tooltip("Direction (eg. X:0, Y:1 for UP)")]
+        public Vector2Int direction;
+
+        [Tooltip("Full wall object (turn-off/onable)")]
+        public GameObject solidWall;
+
+        [Tooltip("Transition/door way (turn-off/onable)")]
+        public GameObject doorOpening;
+    }
+
+    [Header("Door Configuration")]
+    public DoorMapping[] doorMappings;
+
+    public void Initialize(List<Vector2Int> availableDoors)
+    {
+        foreach (DoorMapping doorMapping in doorMappings)
+        {
+            if (doorMapping.solidWall != null)
+            {
+                doorMapping.solidWall.SetActive(true);
+            }
+            if (doorMapping.doorOpening != null)
+            {
+                doorMapping.doorOpening.SetActive(false);
+            }
+        }
+        foreach (Vector2Int doorDir in availableDoors)
+        {
+            foreach (DoorMapping mapping in doorMappings)
+            {
+                if (mapping.direction == doorDir)
+                {
+                    if (mapping.solidWall != null) mapping.solidWall.SetActive(false);
+                    if (mapping.doorOpening != null) mapping.doorOpening.SetActive(true);
+                }
+            }
+        }
+    }
 }
