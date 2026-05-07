8dff6a0b7b28b543799f0c70579536655447f56d
RadKon1 <radkon12345@gmail.com>
2026-05-08 00:13:12 +0200

    Player Stats and Player Manager
    Added player stats scriptable object and player manager class to access values such as xp amount etc.

---
+++ RedCapPolygon/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private PlayerStats _stats;
+
+    private float _currentHealth;
+    private float _maxHealth;
+    private float _currentXP;
+    private int _currentLevel;
+
+    void Awake()
+    {
+        _maxHealth = _stats.BaseMaxHealth;
+        _currentHealth = _maxHealth;
+        _currentLevel = 1;
+    }
+
+    public void AddXP(float amount)
+    {
+        _currentXP += amount;
+        CheckLevelUp();
+    }
+
+    private void CheckLevelUp()
+    {
+        float requiredXP = _stats.XpToLevelUp * Mathf.Pow(_stats.XpMultiplier, _currentLevel - 1);
+
+        if (_currentXP >= requiredXP)
+        {
+            LevelUp(requiredXP);
+        }
+    }
+
+    private void LevelUp(float usedXP)
+    {
+        _currentLevel++;
+        _currentXP -= usedXP;
+
+        _maxHealth += _stats.HealthPerLevel;
+        _currentHealth = _maxHealth; // Full heal
+
+        Debug.Log($"Level Up! Obecny poziom: {_currentLevel}");
+    }
+}
